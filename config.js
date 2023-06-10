46e56ef686e635478519cf5acb51164e4ae815d0
garry2415oo <80236740+garry2415oo@users.noreply.github.com>
2023-06-10 20:00:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDFAy9kxa__akW_V2qQtkxpifyCyQRtQQo",
+  authDomain: "storytellingapp-6690a.firebaseapp.com",
+  projectId: "storytellingapp-6690a",
+  storageBucket: "storytellingapp-6690a.appspot.com",
+  messagingSenderId: "508466006539",
+  appId: "1:508466006539:web:d9fb2e082bba834e68150e"
+  };
+
